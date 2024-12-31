086f3a6567f9aec4512201556f74e27e96576526
Olamidipupo Favour <dipoagboola2019@gmail.com>
2024-12-31 15:13:46 +0100

    update: made set for flask migrate

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ class Config:
     FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
 
     # Database config
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    database_url = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+    if database_url.startswith('postgres://'):
+        database_url = database_url.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # JWT config
