4f3f7094c113cb8dd7864ac9acb0766485eaca08
Olamidipupo Favour <dipoagboola2019@gmail.com>
2024-12-31 14:34:57 +0100

    update: added health endpoint

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from controllers.users import UserController
+from controllers.health import HealthController
 from app import api
 
-api.add_resource(UserController, '/api/users')
-
+api.add_resource(UserController, '/api/v1/users')
+api.add_resource(HealthController, '/api/v1/health')
